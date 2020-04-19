8249ed5c3074a3b9a6103785bfd0325ed3f44d46
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-19 04:40:11 +0300

    5.7(إعطاء لون خاص للنص)

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,9 @@ class HomeScreen extends StatelessWidget {
 //        margin: EdgeInsets.only(left: 50,top: 60),
         margin: EdgeInsets.only(left: 50,right: 150 ,top: 60,bottom: 65),
         padding: EdgeInsets.all(25),
-        child: Text("Welcome Ahmed"),
+        child: Text("Welcome Ahmed", style: TextStyle(
+          color: Colors.white
+        ),),
       ),
     );
   }
