c2ba31188734d2d5b757c2aa4aec37294c4b7c94
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-19 04:26:23 +0300

    5.2(build هي المسؤولة عن بناء الوجدت)

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) { //build هي المسؤولة عن بناء الوجدت
+    // TODO: implement build
+    return null;
+  }
+}
