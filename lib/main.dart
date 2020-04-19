03a30d3cd4f68e95d40bd031bd413cd51dae7a41
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-19 04:47:01 +0300

    5.10(عمل عدد من التصميمات)

--- lib/main.dart
+++ lib/main.dart
@@ -22,16 +22,23 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         color: Colors.teal,
+//        width:350,
+        width:double.infinity,
+//        height: 350,
+        height: double.infinity,
 //        margin: EdgeInsets.all(30),
 //        margin: EdgeInsets.only(left: 50,top: 60),
         margin: EdgeInsets.only(left: 50,right: 150 ,top: 60,bottom: 65),
         padding: EdgeInsets.all(25),
-        child: Text("Welcome Ahmed", style: TextStyle(
+        child: Text("Welcome Ahmed",
+          textAlign: TextAlign.right,
+          style: TextStyle(
           color: Colors.white,
           fontSize: 18,
           fontWeight: FontWeight.bold,
           letterSpacing: 3,
-          decoration: TextDecoration.lineThrough
+          decoration: TextDecoration.lineThrough,
+            height: 3
 
         ),),
       ),
