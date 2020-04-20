224d66ce09ea9708fdb445b971ff85449fb8bb78
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-04-20 16:40:22 +0300

    5.13(استخدام عدة خصائص مع ال column)

--- lib/main.dart
+++ lib/main.dart
@@ -28,20 +28,21 @@ class HomeScreen extends StatelessWidget {
         children: <Widget>[
           Container(
             color: Colors.red,
-            width: 50,
+            width: double.infinity,
             height: 50,
 
           ),
           Container(
             color: Colors.green,
-            width: 50,
+            width: double.infinity,
             height: 50,
           ),
           Container(
             color: Colors.amber,
-            width: 50,
+            width: double.infinity,
             height: 50,
-          )
+          ),
+          Text("Iam Mohammed")
         ],
       )
 
